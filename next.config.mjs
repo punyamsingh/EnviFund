d145ba86a4e23578ab866fcc5e0cff1b5a4f468b
Aayush <aayush12arora@gmail.com>
2024-04-12 10:21:51 +0530

    Added razorpay and shortid dependencies, and updated next.config.mjs with RazorPay keys and Google Sheets credentials

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
 
-export default nextConfig;
+// next.config.mjs
+
+export default {
+  env: {
+    // RazorPay keys
+    RAZORPAY_KEY: '***REMOVED***',
+    RAZORPAY_SECRET: '***REMOVED***',
+//     GOOGLE_SHEETS_PRIVATE_KEY:"***PRIVATE_KEY_REMOVED***\n",
+// GOOGLE_SHEETS_CLIENT_EMAIL:"neelpwm@instant-edudoc.iam.gserviceaccount.com",
+// SPREADSHEET_ID:"1uAGb_f3Fy47ySpE_QNzIQr8jFqSe7pQhVcNN6BLORe0"
+GOOGLE_APPLICATION_CREDENTIALS:'./instant-edudoc.json',
+SHEET_ID:'1uAGb_f3Fy47ySpE_QNzIQr8jFqSe7pQhVcNN6BLORe0'
+  },
+};
